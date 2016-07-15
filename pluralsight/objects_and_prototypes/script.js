6bcf9d55f4c706565ef134dae5f2e953a2c70f49
_michal <sz-m@users.noreply.github.com>
2016-07-15 13:15:05 +0200

    constructor functions

--- pluralsight/objects_and_prototypes/script.js
+++ pluralsight/objects_and_prototypes/script.js
@@ -8,8 +8,24 @@ var cat = {
 
 cat.eyes = 'green';
 
-display('Fluffy is '+cat.color+' cat.');
+display(cat.name+' is '+cat.color+' cat.');
 display(cat.speak());
 
 display('More info about ' + cat.name +':');
 display(cat);
+
+// ------------------------------------------
+
+function Dog(name, color) {
+	this.name = name;
+	this.color = color;
+	this.speak = function() { display('Woaf, woaf!'); }
+}
+
+var rex = new Dog('Rex', 'Black');
+
+display(rex.name+' is '+rex.color+' dog.');
+display(rex.speak());
+
+display('More info about '+rex.name);
+display(rex);
