baf3fbff83ec0a5194de9d97b713b5932394aae3
_michal <sz-m@users.noreply.github.com>
2016-07-17 14:05:16 +0200

    add 03_recursion

---
+++ eloquent-js/exercises/03_recursion/solution.js
@@ -0,0 +1,11 @@
+function isEven(number) {
+	if(number == 0) return true;
+	else if(number == 1) return false;
+	else
+		return isEven((number > 0) ? number-2 : number+2);
+}
+
+console.log(isEven(50));
+console.log(isEven(75));
+console.log(isEven(-1));
+console.log(isEven(-10));
