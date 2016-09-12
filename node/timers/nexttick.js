d60386c02f38de7535b40ccfba2364bb7018048f
_michal <sz-m@users.noreply.github.com>
2016-09-12 15:40:56 +0200

    add process.nextTick example

---
+++ node/timers/nexttick.js
@@ -0,0 +1,21 @@
+var fs = require("fs");
+
+fs.stat("nexttick.js", function(err, stats){
+  if(stats) { console.log("Plik nexttick.js istnieje"); }
+});
+
+setImmediate(function(){
+  console.log("Wykonano natychmiastowy licznik czasu 1");
+});
+
+setImmediate(function(){
+  console.log("Wykonano natychmiastowy licznik czasu 2");
+});
+
+process.nextTick(function(){
+  console.log("Wykonano następny znacznik 1");
+});
+
+process.nextTick(function(){
+  console.log("Wykonano następny znacznik 2");
+});
